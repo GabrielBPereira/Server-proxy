c7081440f7cf4003d90cea216d52dcb025528cad
Gabriel Pereira <gabriellr1908@gmail.com>
2016-04-21 11:18:09 -0300

    Added files via upload

--- filtroConteudo.py
+++ filtroConteudo.py
@@ -1,13 +1,28 @@
+import string
 # coding: utf-8
 
-
 def pesquisaPalavra(dados):
-    ''' Pesquisa palavras dentro da string dados'''
-    # ***************precisa implementar*******************
-    pass
-
-
+	''' Pesquisa palavras dentro da string dados'''
+	try:
+		arq = open('palavras.conf')
+		html = open(dados).read()
+		for palavra in arq:
+			palavra = palavra.replace('\n', '')
+			if palavra in html:
+				return True
+		return False
+	except IOError:
+		return False
+	
 def verificaDominio(host):
-    ''' Verifica se host tem permissão '''
-    # ***************precisa implementar*******************
-    pass
+	'''Verifica se host tem permissão'''
+	try:
+		arq = open('teste.conf')
+		for dominio in arq:
+			dominio = dominio.replace('\n', '')
+			print dominio
+			if dominio == host:
+				return True
+		return False
+	except IOError:
+		return False
